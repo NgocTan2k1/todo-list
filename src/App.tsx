37d8e1019f5721200880da9aaee53ff164af3bc4
NgocTan <phamngoctan2016@gmail.com>
2024-09-11 23:40:11 +0700

    init: adding eslint and prettier for source ver1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import "./App.css";
+import './assets/styles/App.css'
 
 function App() {
-  return <div>Hello World!!!</div>;
+    let string: string = 'abc'
+
+    return <div>Hello World!!!</div>
 }
 
-export default App;
+export default App
