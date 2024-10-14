3da15ea3c2c942dc2d27ad3e53138f18093c3584
NECVN-tanpn <tanpn@nec.vn>
2024-10-14 17:04:12 +0700

    feat: HomePage 1.0

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -5,43 +5,68 @@ import React from 'react';
 import { Button, Divider } from '@mui/material';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import AddIcon from '@mui/icons-material/Add';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 // The customized components
 import BaseNewPage from '../../components/layout/BasePage';
 
 // CSS
 import styles from './HomePage.module.css';
+
 // The stores
 // The customized hooks
 import { useHandleBindingClass } from '../../hooks/useHandleBindingClass';
+import useCommonStores from '../../stores/commonStores';
 
 // The constants
 const HomePage: React.FC = () => {
     // The customized hooks
     const cx = useHandleBindingClass(styles);
 
+    // stores
+    const isExpandMenu = useCommonStores((state) => state.isExpandMenu);
+    const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 1, 1, 1, 1, 1, 1, 9, 2, 1, 2, 4, 5, 6, 3, 9, 9, 2, 1, 1, 5, 5, 4, 1, 2];
     return (
-        <BaseNewPage tailwindCSS={cx('wrapper__home-page', 'flex flex-col h-full w-full')}>
-            <div className={cx('title', 'text-[1.8rem]')}>
+        <BaseNewPage
+            tailwindCSS={cx(
+                'wrapper__home-page',
+                'flex flex-col h-full w-full over overflow-hidden max-[639px]:pt-[1.4rem] max-[639px]:pl-[0.7rem] pt-[2.4rem] pl-[2.4rem]',
+            )}
+        >
+            <div className={cx('title', 'text-[1.8rem] h-auto')}>
                 <h2>Home Page</h2>
             </div>
-            <Divider />
-            <div className={cx('container', 'flex-full')}>
-                <Box className={cx('', 'flex w-full h-full my-4')}>
-                    <Button className={cx('', 'w-[18rem] h-[18rem]')}>
-                        <Box className={cx('', 'flex w-full h-full justify-center items-center')}>
-                            <AddIcon />
-                        </Box>
-                    </Button>
-                    <Paper elevation={3} />
-                    <Paper />
-                    <Paper elevation={3} />
-                    <Paper />
-                    <Paper elevation={3} />
-                    <Paper />
-                    <Paper elevation={3} />
+            <Divider className={cx('container', 'flex-0 min-w-full')} />
+            <div className={cx('container', 'flex-full overflow-y-auto px-2 min-w-full')}>
+                <Box
+                    className={cx(
+                        '',
+                        isExpandMenu
+                            ? 'max-[639px]:w-[24rem] sm:w-[28rem] lg:w-[59rem] xl:w-[90rem] 2xl:w-[121rem] min-[1660px]:w-[152rem]'
+                            : 'max-[639px]:w-[24rem] md:w-[59rem] lg:w-[90rem] 2xl:w-[121rem] min-[1660px]:w-[152rem]',
+
+                        'flex flex-wrap sm:w-[28rem] h-auto sm:justify-center md:justify-start gap-x-12 gap-y-12 my-4 mx-auto',
+                    )}
+                >
+                    {array.map((_, index) => (
+                        <Paper
+                            key={index}
+                            component={Button}
+                            className={cx(
+                                '',
+                                'max-[639px]:!min-w-[24rem] max-[640px]:!min-h-[20rem] !min-w-[28rem] !min-h-[26rem] !rounded-3xl  hover:bg-neutral-100',
+                            )}
+                            elevation={3}
+                        >
+                            <AddCircleOutlineIcon className={cx('item__icon', '!w-[4rem] !h-[4rem]')} />
+                        </Paper>
+                    ))}
                 </Box>
+                <Stack spacing={2}>
+                    <Pagination className={cx('pagination', '!m-4')} count={5} showFirstButton showLastButton />
+                </Stack>
             </div>
         </BaseNewPage>
     );
--- src/pages/profile/ProfilePage.tsx
+++ src/pages/profile/ProfilePage.tsx
@@ -1,32 +1,56 @@
 import React from 'react';
 
 // firebase
-import { getAuth, signOut } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import { firebaseApp } from '../../firebase';
 
 // The components
 // The customized components
 import BaseNewPage from '../../components/layout/BasePage';
+import Divider from '@mui/material/Divider';
+import Badge from '@mui/material/Badge';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 
 // CSS
+import styles from './ProfilePage.module.css';
 // The stores
 // The customized hooks
+import { useHandleBindingClass } from '../../hooks/useHandleBindingClass';
+import moment from 'moment';
 // The constants
 
 const ProfilePage: React.FC = () => {
+    // The customized hooks
+    const cx = useHandleBindingClass(styles);
+
     // firebase
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const auth = getAuth(firebaseApp);
 
     return (
-        <BaseNewPage>
-            <div className="text-9xl bg-blue-500 text-white p-5">Profile Page</div>{' '}
-            <button
-                onClick={() => {
-                    signOut(auth);
-                }}
-            >
-                SignOut
-            </button>
+        <BaseNewPage
+            tailwindCSS={cx(
+                'wrapper__profile-page',
+                'flex flex-col h-full w-full over overflow-hidden max-[639px]:p-[1.4rem] max-[639px]:p-[0.7rem] p-[2.4rem] bg-[#F9F9F9]',
+            )}
+        >
+            <div className={cx('title', 'relative text-[1.8rem] h-auto bg-white p-1 rounded-xl')}>
+                <h2 className={cx('', 'pl-[1.2rem]')}>Welcome, My Name</h2>
+                <p className={cx('', 'text-[#ADA7A7] my-1 text-[1.2rem] pl-[1.4rem]')}>
+                    {`${moment(new Date()).format('ddd, DD MMM YYYY')}`}
+                </p>
+                <Badge
+                    className={cx('', '!absolute w-[2.4rem] h-[2.4rem] top-[50%] translate-y-[-50%] right-[1.2rem]')}
+                    badgeContent={4}
+                    color="primary"
+                >
+                    <NotificationsNoneIcon className={cx('', '!w-[2.4rem] !h-[2.4rem] hover:cursor-pointer')} color="action" />
+                </Badge>
+                <Divider className={cx('container', 'flex-0 min-w-full ')} />
+            </div>
+            <div
+                className={cx('container', 'flex-full overflow-y-auto min-w-full md:mt-[2.4rem] bg-white shadow-xl rounded-xl')}
+            ></div>
         </BaseNewPage>
     );
 };
